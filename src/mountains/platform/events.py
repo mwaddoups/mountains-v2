b769ce0e12f700a6fd807fb00f0af3184a76975a
Mark Waddoups <mwaddoups@gmail.com>
2024-12-25 22:02:57 +0000

    Basic basic event page

---
+++ src/mountains/platform/events.py
@@ -0,0 +1,54 @@
+import datetime
+
+from quart import Blueprint, current_app, render_template, request
+
+from mountains.errors import MountainException
+
+from ..events import Event, events
+
+
+def events_repo():
+    return events(current_app.config["DB_NAME"])
+
+
+def event_routes(blueprint: Blueprint):
+    @blueprint.route("/events", methods=["GET", "POST"])
+    async def events():
+        if request.method == "POST":
+            form_data = await request.form
+
+            event = Event.from_new_event(
+                title=form_data["title"],
+                description=form_data["description"],
+                event_dt_str=form_data["event_dt"],
+                event_end_dt_str=form_data["event_end_dt"],
+                event_type_str=form_data["event_type"],
+                max_attendees_str=form_data["max_attendees"],
+            )
+            events_repo().insert(event)
+
+            # TODO: Audit the event
+
+        # TODO: reasonable day threshold, old and new events
+        events = sorted(
+            [
+                e
+                for e in events_repo().list()
+                if e.event_dt > datetime.datetime.now() - datetime.timedelta(days=4)
+            ],
+            key=lambda e: e.event_dt,
+        )
+        return await render_template("platform/events.html.j2", events=events)
+
+    @blueprint.route("/events/add")
+    @blueprint.route("/events/edit/<id>")
+    async def edit_event(id: str | None = None):
+        if id is not None:
+            event = events_repo().get(id=id)
+            if event is None:
+                raise MountainException("Event not found!")
+        else:
+            event = None
+        return await render_template(
+            "platform/event.edit.html.j2", event=event, event_types=Event.event_types()
+        )
--- src/mountains/platform/routes.py
+++ src/mountains/platform/routes.py
@@ -2,25 +2,17 @@ import logging
 
 from quart import (
     Blueprint,
-    current_app,
     g,
     redirect,
     render_template,
-    request,
     session,
     url_for,
 )
 
-from mountains.errors import MountainException
-
-from ..events import Event, events
+from .events import event_routes
 from .members import member_routes, users_repo
 
 logger = logging.getLogger(__name__)
-
-
-def events_repo():
-    return events(current_app.config["DB_NAME"])
 
 
 def routes(blueprint: Blueprint):
@@ -43,37 +35,6 @@ def routes(blueprint: Blueprint):
     async def home():
         return await render_template("platform/home.html.j2")
 
-    # Add the member routes
     member_routes(blueprint)
 
-    @blueprint.route("/events", methods=["GET", "POST"])
-    async def events():
-        if request.method == "POST":
-            form_data = await request.form
-
-            event = Event.from_new_event(
-                title=form_data["title"],
-                description=form_data["description"],
-                event_dt_str=form_data["event_dt"],
-                event_end_dt_str=form_data["event_end_dt"],
-                event_type_str=form_data["event_type"],
-                max_attendees_str=form_data["max_attendees"],
-            )
-            events_repo().insert(event)
-
-            # TODO: Audit the event
-        events = events_repo().list()
-        return await render_template("platform/events.html.j2", events=events)
-
-    @blueprint.route("/events/add")
-    @blueprint.route("/events/edit/<id>")
-    async def edit_event(id: str | None = None):
-        if id is not None:
-            event = events_repo().get(id=id)
-            if event is None:
-                raise MountainException("Event not found!")
-        else:
-            event = None
-        return await render_template(
-            "platform/event.edit.html.j2", event=event, event_types=Event.event_types()
-        )
+    event_routes(blueprint)
