82b2c9de203ebe82a3272b539dd5858c7011b596
Mark Waddoups <mwaddoups@gmail.com>
2025-01-27 14:25:36 +0000

    Hand off pid for reload

--- prod.sh
+++ prod.sh
@@ -4,4 +4,4 @@ set -a
 source prod.env
 set +a
 
-/root/.local/bin/uv run gunicorn --workers=4 -k gthread --threads 4 -b 127.0.0.1:8000 'mountains:create_app()'
+exec /root/.local/bin/uv run gunicorn --workers=4 -k gthread --threads 4 -b 127.0.0.1:8000 'mountains:create_app()'
