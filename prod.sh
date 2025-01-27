9614255c2713c60f70a912a021108196955bc719
Mark Waddoups <mwaddoups@gmail.com>
2025-01-27 10:35:25 +0000

    Bind on TCP

--- prod.sh
+++ prod.sh
@@ -4,4 +4,4 @@ set -a
 source prod.env
 set +a
 
-uv run gunicorn --workers=4 -k gthread --threads 4 -b /tmp/gunicorn.sock 'mountains:create_app()'
+uv run gunicorn --workers=4 -k gthread --threads 4 -b ['127.0.0.1:8000'] 'mountains:create_app()'
