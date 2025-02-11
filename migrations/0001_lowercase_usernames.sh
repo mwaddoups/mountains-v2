ba46a0d890a44371d1b7e444dec786432124da64
Mark Waddoups <mwaddoups@gmail.com>
2025-02-11 13:25:46 +0000

    Update migration

--- migrations/0001_lowercase_usernames.sh
+++ migrations/0001_lowercase_usernames.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Ensures all emails are lowercase, so comparisons when getting make sense
 cp $1 $1.bak
-python -m sqlite3 $1 'UPDATE users SET email = LOWER(email);'
+uv run python -m sqlite3 $1 'UPDATE users SET email = LOWER(email);'
