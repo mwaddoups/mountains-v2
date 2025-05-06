e5a7742de18cbae5d496ed5d63c482ed66c58805
Mark Waddoups <mwaddoups@gmail.com>
2025-05-06 13:19:22 +0100

    Update prod settings to restart now and then for memory leak

--- prod.sh
+++ prod.sh
@@ -4,4 +4,4 @@ set -a
 source prod.env
 set +a
 
-exec /root/.local/bin/uv run gunicorn --workers=4 -k gthread --threads 4 -b 127.0.0.1:8000 'mountains:create_app()'
+exec /root/.local/bin/uv run gunicorn --workers=4 --max-requests 100 --max-requests-jitter 10 -k gthread --threads 4 -b 127.0.0.1:8000 'mountains:create_app()'
