954b604814aaa555717b4fe6c0724464bb38e57e
Mark Waddoups <mwaddoups@gmail.com>
2025-08-17 16:00:29 +0100

    Remove unused old scripts

--- scripts/download_old_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-
-ssh -t mountains "cd mountains/mountains && DJANGO_APP_STAGE=prod /root/.local/bin/poetry run python manage.py dumpdata -e contenttypes -e auth.Permission --indent 2 > datadump.json"
-scp mountains:~/mountains/mountains/datadump.json datadump.json
--- scripts/download_old_media.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-
-rsync -av --delete -e ssh mountains:/var/www/html/media/uploads/profile/ src/mountains/static/profile/
-rsync -av --delete -e ssh mountains:/var/www/html/media/uploads/photos/ src/mountains/static/uploads/photos/
